ff526b6c025ed29ab945cc6f8cf8680c7672c13a
Alexis <alexis.z@lilo.org>
2022-02-18 17:13:10 +0100

    Add P2C1-Solution

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,6 +1,8 @@
 import Card from '../../components/Card'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import { useEffect, useState } from 'react'
+import { Loader } from '../../utils/style/Atoms'
 
 const CardsContainer = styled.div`
   display: grid;
@@ -26,6 +28,11 @@ const PageSubtitle = styled.h2`
   padding-bottom: 30px;
 `
 
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 const freelanceProfiles = [
   {
     name: 'Jane Doe',
@@ -42,13 +49,43 @@ const freelanceProfiles = [
 ]
 
 function Freelances() {
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [freelancersList, setFreelancersList] = useState([])
+
+  useEffect(() => {
+    async function fetchFreelances() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/freelances`)
+        const { freelancersList } = await response.json()
+        setFreelancersList(freelancersList)
+      } catch (err) {
+        console.log('===== error =====', err);
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchFreelances()
+  }, [])
+
+  if (error) {
+    return <span>Oups il y a eu un problème dans la matrice</span>
+  }
+
   return (
     <div>
       <PageTitle>Trouvez votre prestataire</PageTitle>
       <PageSubtitle>
         Chez Shiny nous réunissons les meilleurs profils pour vous.
       </PageSubtitle>
-      <CardsContainer>
+      { isDataLoading ? (
+        <LoaderWrapper>
+          <Loader />
+        </LoaderWrapper>
+      ) : (
+        <CardsContainer>
         {freelanceProfiles.map((profile, index) => (
           <Card
             key={`${profile.name}-${index}`}
@@ -57,6 +94,7 @@ function Freelances() {
           />
         ))}
       </CardsContainer>
+      )}
     </div>
   )
 }
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,24 +1,99 @@
-import { Link, useParams } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
 
 function Survey() {
-    const { questionNumber } = useParams() // hook (récupère le paramètre depuis l'URL)
-    const questionNumberInt = parseInt(questionNumber) // parseInt (convertit une chaîne de caractères en nombre entier) : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-    const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
-    const nextQuestionNumber = questionNumberInt + 1
+  const { questionNumber } = useParams() // hook (récupère le paramètre depuis l'URL)
+  const questionNumberInt = parseInt(questionNumber) // parseInt (convertit une chaîne de caractères en nombre entier) : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+  const nextQuestionNumber = questionNumberInt + 1
 
-    return (
-        <div>
-            <h1>Questionnaire</h1>
-            <h2>Question {questionNumber}</h2>
+  const [surveyData, setSurveyData] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
 
-            <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-            {questionNumberInt === 10 ? (
-                <Link to="/results">Résultats</Link>
-            ) : (
-                <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-            )}
-        </div>
-    )
+  // Cette syntaxe permet aussi bien de faire des calls API.
+  // Mais pour utiliser await dans une fonction, il faut que celle-ci soit async (pour asynchrone).
+  /* useEffect(() => {
+    fetchData()
+    setDataLoading(true)
+    fetch(`http://localhost:8000/survey`).then((response) =>
+      response.json().then(({ surveyData }) => {
+        setSurveyData(surveyData)
+        setDataLoading(false)
+      })
+    ) 
+  }, []) */
+  
+  // Comme la fonction passée à useEffect ne peut pas être asynchrone,
+  // il faut utiliser une fonction qui est appelée dans useEffect et déclarée en dehors, comme ici 👇.
+  useEffect(() => {
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json() // { surveyData } est une propriété de l'objet qui est retourné donc entre {}. Il est aussi parsé avec .json()
+        setSurveyData(surveyData)
+      } catch (err) {
+        console.log('===== error =====', err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchSurvey()
+  }, [])
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
+
+  return (
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      )}
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
+  )
 }
 
 export default Survey
